82ef473d18b73b0823241e24029b6a4d6fcd6478
nabetama <mao.nabeta@gmail.com>
2016-10-29 00:32:30 +0900

    sample.

--- go-lang/ws/app.go
+++ go-lang/ws/app.go
@@ -1,26 +1,44 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"log"
 	"net/http"
 
 	"golang.org/x/net/websocket"
 )
 
-func echoHandler(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+func Echo(ws *websocket.Conn) {
+	var err error
 
+	for {
+		var reply string
+
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Cant't receive")
+			break
+		}
+
+		fmt.Println("Receive back from client " + reply)
+
+		msg := "Received : " + reply
+		fmt.Println("Sending to client: " + msg)
+
+		if err = websocket.Message.Send(ws, msg); err != nil {
+			fmt.Println("Cant't send")
+			break
+		}
+	}
 }
 
 func main() {
+	http.Handle("/", websocket.Handler(Echo))
 	http.HandleFunc("/echo",
 		func(w http.ResponseWriter, r *http.Request) {
-			s := websocket.Server{Handler: websocket.Handler(echoHandler)}
+			s := websocket.Server{Handler: websocket.Handler(Echo)}
 			s.ServeHTTP(w, r)
 		})
-
-	http.Handle("/", http.FileServer(http.Dir("./")))
 	if err := http.ListenAndServe(":9999", nil); err != nil {
-		panic(err)
+		log.Fatal("Listen Serve	", err)
 	}
 }
