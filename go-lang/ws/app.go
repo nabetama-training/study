7609f222a95004ed8fb32c94d137578ee83c6fb4
nabetama <mao.nabeta@gmail.com>
2016-10-28 23:11:39 +0900

    cross origin.

--- go-lang/ws/app.go
+++ go-lang/ws/app.go
@@ -13,7 +13,12 @@ func echoHandler(ws *websocket.Conn) {
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(echoHandler))
+	http.HandleFunc("/echo",
+		func(w http.ResponseWriter, r *http.Request) {
+			s := websocket.Server{Handler: websocket.Handler(echoHandler)}
+			s.ServeHTTP(w, r)
+		})
+
 	http.Handle("/", http.FileServer(http.Dir("./")))
 	if err := http.ListenAndServe(":9999", nil); err != nil {
 		panic(err)
